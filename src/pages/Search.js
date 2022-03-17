18ea71fb4a70f08c38bbb07c2f3e484b5797363e
persiopeo <persiopeo@hotmail.com>
2022-03-17 13:22:18 -0300

    Requisito-05

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,14 +1,78 @@
 import React from 'react';
+import Carregando from '../componets/Carregando';
 import Header from '../componets/Header';
 
 class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      botao: true,
+      loading: false,
+      cantor: '',
+
+    };
+  } // fim do constructor
+
+  // inicio de funções
+  onInputChange = ({ target }) => {
+    const { name } = target;
+    const { value } = target;
+
+    this.setState(
+      { [name]: value },
+      () => this.validaTamanho(),
+    );
+  }
+
+  validaTamanho = () => {
+    const tamanhoMin = 2;
+    const { cantor } = this.state;
+    const maiorQue2 = cantor.length >= tamanhoMin;
+    this.setState(
+      maiorQue2
+        ? { botao: false }
+        : { botao: true },
+
+    );
+  }
+
+  fomrRender = () => {
+    const { onInputChange } = this;
+    const { botao, cantor } = this.state;
+    return (
+      <section>
+        <form onSubmit={ (e) => e.preventDefault() }>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            value={ cantor }
+            name="cantor"
+            onChange={ onInputChange }
+          />
+          <input
+            data-testid="search-artist-button"
+            type="button"
+            value="Pesquisar"
+            disabled={ botao }
+          />
+
+        </form>
+      </section>
+
+    );
+  }// fim do formRender
+
+  // fim das funções
   render() {
+    const { loading } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <h2>Pagina de search</h2>
-      </div>
 
+        {loading ? <Carregando /> : this.fomrRender()}
+
+      </div>
     );
   }
 }
